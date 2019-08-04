e096102a086874d27393920567efe9fa7c029ad6
kobayashi takuya <kobataku0629@gmail.com>
2019-08-04 21:19:01 +0900

    create chat server

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module server
 
 go 1.12
+
+require (
+	github.com/googollee/go-socket.io v1.0.1
+	github.com/rs/cors v1.6.0
+)
--- server/main.go
+++ server/main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-    "fmt"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/rs/cors"
 )
 
 func main() {
-    fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection", so.Id())
+		so.Join("room")
+		log.Println("join room", so.Id())
+		so.On("chat", func(msg string) {
+			log.Println("chat", so.Id(), msg)
+			server.BroadcastTo("room", "chat", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect", so.Id())
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	mux.Handle("/socket.io/", server)
+
+	corsOptions := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:8081"},
+		AllowCredentials: true,
+		Debug:            false,
+	})
+	handler := corsOptions.Handler(mux)
+
+	log.Println("Serving at localhost:8080...")
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
