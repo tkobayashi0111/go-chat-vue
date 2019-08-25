ae419ba96ea2cccc74e873093c1610399bc1143a
kobayashi takuya <kobataku0629@gmail.com>
2019-08-25 18:48:26 +0900

    メッセージ入力エリアを下固定にする

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,12 +1,13 @@
 <template>
   <v-row>
     <v-col lg="6" offset-lg="3" md="12">
-      <div>
+      <div :style="{ overflowY: 'auto', height: getContentAreaHeight() }">
         <template v-for="(message, i) in messages">
           <conversation :key="`message-${i}`" :message="message" />
         </template>
       </div>
       <v-textarea
+        ref="textarea"
         v-model="message"
         append-icon="mdi-send"
         auto-grow
@@ -21,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 import io from 'socket.io-client'
 
 import Conversation from '@/components/Conversation.vue'
@@ -38,9 +39,12 @@ export default class Index extends Vue {
 
   private messages: string[] = []
 
+  private textarea: null | HTMLElement = null
+
   mounted() {
     this.socket = io('/chat')
     this.socket.on('chat', this.onReceiveChat)
+    this.textarea = (this.$refs.textarea as Vue).$el as HTMLElement
   }
 
   onReceiveChat(message) {
@@ -54,6 +58,24 @@ export default class Index extends Vue {
 
     this.socket.emit('chat', this.message)
     this.message = ''
+  }
+
+  @Watch('message')
+  private getContentAreaHeight() {
+    if (this.textarea) {
+      const textareaHeight = this.getElementHeight(this.textarea)
+      const containerPadding = 48
+      return `calc(100vh - ${textareaHeight}px - ${containerPadding}px)`
+    } else {
+      return '0'
+    }
+  }
+
+  private getElementHeight(el: HTMLElement) {
+    const style = window.getComputedStyle(el)
+    const margin =
+      parseInt(style.marginTop || '0') + parseInt(style.marginBottom || '0')
+    return el.offsetHeight + margin
   }
 }
 </script>
